efe79b809eedebb4694692d0e50f63bfb152e23e
sc232jm <145260982+sc232jm@users.noreply.github.com>
2024-01-29 15:02:39 +0000

    finished

--- tasks/task.c
+++ tasks/task.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     int num1, num2;
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    if (argc != 3) {        
+        printf("Please ensure you're passing only two additional arguments");
+        return -1;
+    }
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
 
-    // Calculate and print the sum
+    if (num1 == 0 || num2 == 0) {
+        printf("Invalid input: Please enter non-zero numbers\n");
+        return -1;
+    }
+
     int sum = num1 + num2;
     printf("Sum: %d\n", sum);
 
-    return 0; // Exit successfully
+    return 0;
 }
